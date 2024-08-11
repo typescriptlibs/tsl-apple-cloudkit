087c14ce9e109696ecc7353be93f336950647ce2
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2024-08-11 18:41:30 +0200

    Fix Vue test demo

--- AUTHORS.md
+++ AUTHORS.md
@@ -1,3 +1,4 @@
 Apple CloudKit TypeScript Library is authored by:
 * c0rv0s
+* pcccccc
 * Sophie Bremer
--- tests/vue_vite_demo/src/components/HelloWorld.vue
+++ tests/vue_vite_demo/src/components/HelloWorld.vue
@@ -8,16 +8,16 @@
 
 <script setup lang="ts">
 
-import { onMounted, ref } from "vue";
+import { onMounted, ref } from 'vue';
 import * as CloudKit from 'tsl-apple-cloudkit';
-import { UserIdentity } from "tsl-apple-cloudkit";
-let container: CloudKit.Container | undefined = undefined
-let messageInfo = ref("")
+import { CKError, UserIdentity } from 'tsl-apple-cloudkit';
+let container: CloudKit.Container | undefined = undefined;
+let messageInfo = ref('');
 
 onMounted(() => {
-  let cloudKit = window.CloudKit
-  const containerIdentifier = "iCloud.org.netzkonform.CloudKitTS"
-  const apiToken = "825628f17af22927b7e8901ad7ec3db8ada33ce8c31e870b4f09d9e3d3a4b13c"
+  let cloudKit = window.CloudKit;
+  const containerIdentifier = 'iCloud.org.netzkonform.CloudKitTS';
+  const apiToken = '825628f17af22927b7e8901ad7ec3db8ada33ce8c31e870b4f09d9e3d3a4b13c';
   const configuredCloudKit = cloudKit.configure({
     containers: [{
       containerIdentifier: containerIdentifier,
@@ -25,29 +25,29 @@ onMounted(() => {
         apiToken: apiToken,
         persist: true,
         signInButton: {
-          id: "apple-sign-in-button",
-          theme: "dark"
+          id: 'apple-sign-in-button',
+          theme: 'dark'
         },
         signOutButton: {
-          id: "apple-sign-out-button",
-          theme: "dark"
+          id: 'apple-sign-out-button',
+          theme: 'dark'
         }
       },
-      apnsEnvironment: "development",
-      environment: "development"
+      apnsEnvironment: 'development',
+      environment: 'development'
     }],
   });
 
-  container = configuredCloudKit.getContainer(containerIdentifier)
-  container?.setUpAuth()
+  container = configuredCloudKit.getContainer(containerIdentifier);
+  container?.setUpAuth();
   container?.whenUserSignsIn()
       .then((userIdentity: UserIdentity) => {
           messageInfo.value = userIdentity.userRecordName
       })
-      .catch((error: CKError) => {
-          messageInfo.value = error.toString()
+      .catch((error?: CKError) => {
+          messageInfo.value = '' + error;
       });
-})
+});
 
 </script>
 
